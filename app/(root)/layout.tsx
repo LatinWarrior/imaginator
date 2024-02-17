6bf713e64f2eb5aa361ca9f4f56ef2794693da2b
Luis Blanco <luisblanco@Luiss-MacBook-Pro-2.local>
2024-02-17 15:58:37 -0600

    Finish web view

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
 import { ReactNode } from 'react';
 
+import Sidebar from '@/components/shared/Sidebar';
+
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
         <main className='root'>
+            <Sidebar />
+            {/* <MobileNav /> */}
+
             <div className='root-container'>
                 <div className='wrapper'>{children}</div>
             </div>
